f74fa9c4d0e94378bf597e07f74219f967ac567c
Juan J. Garcia <j2gl.edu@gmail.com>
2019-05-06 19:15:18 -0600

    Add cicular list

---
+++ src/main/java/edu/advanced/unit5/model/Node.java
@@ -0,0 +1,29 @@
+package edu.advanced.unit5.model;
+
+public class Node {
+
+    private int value;
+    private Node next;
+
+    public Node() {
+        this.value = 0;
+        this.next = null;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+}
